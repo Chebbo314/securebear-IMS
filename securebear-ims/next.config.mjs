f5557b870a312912e88b140bb84864fe1ed9c416
Felix <felix.zaers@web.de>
2024-11-08 18:03:42 +0100

    add export

--- securebear-ims/next.config.mjs
+++ securebear-ims/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// next.config.mjs
+export default {
+    output: 'export', // Required for static export
+    trailingSlash: true, // Optional: Adds a trailing slash to URLs (e.g. /about/ instead of /about)
+  }
+  
